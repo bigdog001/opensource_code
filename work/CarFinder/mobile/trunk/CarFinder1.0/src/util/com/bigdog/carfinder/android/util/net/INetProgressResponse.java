22c83dc0b5afd3fabda6656e02b183ef057802ad
bigdog001 <294356967@qq.com>
2014-07-05 13:58:46 -0400

    Revert "init"
    This reverts commit eac71159e86b00d0a199998b57185282f8826f94.

--- work/CarFinder/mobile/trunk/CarFinder1.0/src/util/com/bigdog/carfinder/android/util/net/INetProgressResponse.java
+++
@@ -1,6 +0,0 @@
-package  com.bigdog.carfinder.android.util.net;
-public interface INetProgressResponse extends INetResponse {
-	
-	public abstract void progress(int progress);
-
-}
--- work/facemail/mobile/trunk/src/util/com/facemail/mobile/android/util/net/INetProgressResponse.java
+++
@@ -1,7 +0,0 @@
-package com.facemail.mobile.android.util.net;
-
-public interface INetProgressResponse extends INetResponse {
-	
-	public abstract void progress(int progress);
-
-}
